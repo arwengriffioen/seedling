6c84d98ebe4b7ad1fd88bb0d445f17363ed47f3a
Arwen Griffioen <agriffioen@datateamgpu1.numbats.us-west-1a.aws.zdsys.com>
2017-08-10 01:03:49 +0000

    adding the install scripts

---
+++ seedling.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+clear
+
+# Install Oh My Zsh
+echo "NO PASSWORD" | bash <(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
+cat lib/zshrc_additions >> ~/.zshrc
+source ~/.zshrc
+
+# Install Ultimate VIM
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# Set up vim for awesomeness
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cat lib/vimrc_template >> ~/.vimrc
