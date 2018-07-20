50193292acfd95526d8fd1426d5b9c22c72fca9c
Arwen Griffioen <agriffioen@zendesk.com>
2018-07-20 10:02:26 +1000

    trying to fix things

--- seedling.sh
+++ seedling.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 clear
 
-# Install Oh My Zsh
-# Download Zsh
-sg aha-team -c "aws s3 sync s3://zendesk-datascientists-intermediate-east/zsh/ ~/.zsh/ && chmod +x ~/.zsh/zsh && chmod -R g-w,o-w ~/.zsh/share"
-export PATH=$HOME/.zsh:$PATH
-echo "export PATH=$HOME/.zsh:$PATH" >> ~/.bashrc
+isZendesk=false
 
-# Install Oh My Zsh and set theme
+# Download Zsh
+if ! [ -x "$(command -v zsh)" ]; then
+  sg aha-team -c "aws s3 sync s3://zendesk-datascientists-intermediate-east/zsh/ ~/.zsh/ && chmod +x ~/.zsh/zsh && chmod -R g-w,o-w ~/.zsh/share"
+  export PATH=$HOME/.zsh:$PATH
+  echo "export PATH=$HOME/.zsh:$PATH" >> ~/.bashrc
+  isZendesk=true
+fi
+
+# Install Oh My Zsh and enable Powerlevel 9k
 echo "NO PASSWORD" | bash <(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
-cat ~/code/seedling/lib/zshrc_additions >> ~/.zshrc
+cat lib/zshrc_additions >> ~/.zshrc
 sed -i 's/ZSH_THEME=.*/ZSH_THEME="blinks"/' ~/.zshrc
-source ~/.zshrc
+
+if [ $isZendesk ]; then
+ sed -i "1ifpath=($HOME/.zsh/share/zsh/5.5.1-dev-0/functions)" ~/.zshrc
+fi
+zsh -c 'source ~/.zshrc'
 
 # Install Ultimate VIM
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
@@ -20,7 +28,9 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 # Set up vim for awesomeness
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cat ~/code/seedling/lib/vimrc_template >> ~/.vimrc
+cat lib/vimrc_template > ~/.vimrc
 
-cat ~/code/seedling/lib/bash_additions zsh >> ~/.bashrc
+vim +PluginInstall +qall
 source ~/.bashrc
+
+echo "You're All Done :)"
